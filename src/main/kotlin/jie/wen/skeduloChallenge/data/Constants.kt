db91bd6d063f6e093f89804c0066ea4a58722c0d
Wen <jie.wen@live.hk>
2022-04-13 14:16:09 +0800

    0.0.2-RELEASE update
    1. Use Date for start and finish instead of String
    2. Use sorted array list instead of priority queue, use binary search instead of linear search to find next performance
    3. improve the performance

--- src/main/kotlin/jie/wen/skeduloChallenge/data/Constants.kt
+++ src/main/kotlin/jie/wen/skeduloChallenge/data/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val DOT: String = "."
     const val OUTPUT_FILE_NAME_SUFFIX = "optimal"
     const val USER_DIR = "user.dir"
-    const val SHOW_TEST_RESULT = true
+    const val SHOW_TEST_RESULT = false
 }
